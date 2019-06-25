207c1143cbaf49b98474d19014521bcc8ed8cb3c
ICE <donfeng97@csu.fullerton.edu>
2019-06-24 22:07:07 -0700

    put bootstrap in

--- php/ssn.php
+++ php/ssn.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>CPSC332 - Project 1 Professors</title>
 	<link href="project2.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
 
 <?php	
