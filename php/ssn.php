0284dba69bb263156117a835811b2ba595b028fc
ice <donfeng97@csu.fullerton.edu>
2019-06-24 23:31:05 -0700

    finally ssn good

--- php/ssn.php
+++ php/ssn.php
@@ -47,31 +47,40 @@ $result = $con->query($sql);
     <div class="input_data1">
 		<div class="container2">
 		
-<?php
-if ($result->num_rows > 0) {
-	echo "<table calss=\"table\">"; // start a table tag in the HTML
 
-	echo "<thead>
-	<tr><th scope=\"col\">CTITLE</th>	<th scope=\"col\">CLASSROOM</th>	
-			  <th scope=\"col\">MEETING_DAYS</th>	<th scope=\"col\">TIME</th></tr>
-		</thead>";
-	 while($row = $result->fetch_assoc()) {
-        echo "<tbody>
-		<tr><td>" . $row["CTITLE"] . "</td>" 
-			."<td>" . $row["CLASSROOM"] . "</td>"
-			."<td>" . $row["MEETING_DAYS"] . "</td>"
-			."<td>" . $row["BEGINNING_TIME"] 
-			. ' - '. $row["ENDING_TIME"] .  "</td></tr>
-			</tbody>";	
-    }
-	echo "</table>"; //Close the table in HTML  
-} else {
+
+
+<?php
+if ($result->num_rows > 0) {?>
+		<table class="table"> 
+	<thead>	
+      <tr> 
+          <th scope="col">Title </th> 
+          <th scope="col">Classroom </th> 
+	  <th scope="col">Meeting_date </th> 
+          <th scope="col">Time</th>
+      </tr>
+	</thead>
+	<tbody>
+	 <?php while($row = $result->fetch_assoc()) { ?>
+				<tr>
+				<td><?php echo  $row["CTITLE"] ; ?></td>
+			 <td> <?php echo  $row["CLASSROOM"] ;?></td>
+			 <td><?php echo $row["MEETING_DAYS"] ;?></td>
+			 <td><?php echo $row["BEGINNING_TIME"] 
+			 . ' - '. $row["ENDING_TIME"] ;?></td>
+			 </tr>
+			 <?php } ?>
+			</tbody>	
+	</table>
+<?php }
+else {
     echo "0 results";
-}
+} ?>
+<?php
 $con->close();
 ?>
-			
-		</div>
+			</div>
 	</div>
 	  
   	</section>
@@ -81,6 +90,13 @@ $con->close();
     <p>yifei feng - lambert liu - shijie feng </p>
   </div>
 		</div>
+
+
+		 <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</body>
 </html>
 
