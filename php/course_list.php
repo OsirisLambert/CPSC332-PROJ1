5b1bd5abd7d018bdad81a6155edd6af72666911b
ice <donfeng97@csu.fullerton.edu>
2019-06-24 23:38:25 -0700

    start others

--- php/course_list.php
+++ php/course_list.php
@@ -1,6 +1,15 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>CPSC332 - Project 1 Professors</title>
+	<link href="project2.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
 
-	
+<?php
 $CNO = $_POST['CNO'];
 $dbServername = "mariadb";
 $dbUsername = "cs332z7";
@@ -19,7 +28,24 @@ $sql = "SELECT *, COUNT(*)
 		FROM  ENROLLMENT E, SECTION S
 		WHERE S.CNO = $CNO AND S.CNO = E.CNO AND S.SNO = E.SNO ";
 $result = $con->query($sql);
+?>
 
+<body>
+	<div class="container">
+	  <header> <a href="http://ecs.fullerton.edu/~cs332z7">
+    <h4 class="logo">Home</h4>
+	  </a>
+  </header>
+	<section class="title" id="title">
+    <h2 class="title_header">Result<font color="#717070">Output</font></h2>
+    <p class="tagline">professors</p>
+  </section>
+	<section class="input" id="input">
+	  
+    <div class="input_data1">
+		<div class="container2">
+
+<?php
 if ($result->num_rows > 0) {
 	echo "<table border = 1>"; // start a table tag in the HTML
 	echo "<tr><td>SNO</td>	<td>CLASSROOM</td>	
@@ -38,3 +64,13 @@ if ($result->num_rows > 0) {
 }
 $con->close();
 ?>
+</div>
+</div>
+
+	 <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+</html>
